2459a1d2c5fdbc5a63dd18b9d0442ea23d92798b
code-commit-at-711830799325 <code-commit-at-711830799325>
2020-12-11 20:07:31 -0800

    remove routes from server.js

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const userRouter = require('./routes/users.js');
-const exerciseRouter = require('./routes/exercises.js');
+// const userRouter = require('./routes/users.js');
+// const exerciseRouter = require('./routes/exercises.js');
 
 const path = require('path');
 
